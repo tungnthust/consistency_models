265723f029107ca23693566aa9594108533d418f
tungnthust <thanhtungbka1@gmail.com>
2023-04-24 16:08:42 +0700

    update

--- cm/karras_diffusion.py
+++ cm/karras_diffusion.py
@@ -344,7 +344,8 @@ class KarrasDenoiser:
                 for x in self.get_scalings_for_boundary_condition(sigmas)
             ]
         rescaled_t = 1000 * 0.25 * th.log(sigmas + 1e-44)
-        model_output = model(c_in * x_t, rescaled_t, **model_kwargs)
+        with th.backends.cuda.sdp_kernel(enable_flash=False) as disable :
+            model_output = model(c_in * x_t, rescaled_t, **model_kwargs)
         denoised = c_out * model_output + c_skip * x_t
         return model_output, denoised
 
